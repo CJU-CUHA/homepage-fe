5645bf952eed10c24c18d13ae824adaaf1ab6d03
test <freetime483@naver.com>
2025-04-10 10:32:15 +0900

    .

--- find_di/pages/_d.js
+++ find_di/pages/_d.js
@@ -12,30 +12,3 @@ document.querySelector('.click-me').addEventListener('click', () => {
       modal.style.display = 'none';
     }
   });
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    const authToggle = document.getElementById("authToggle");
-  
-    let isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
-  
-    updateAuthText();
-  
-    authToggle.addEventListener("click", function (e) {
-      e.preventDefault(); 
-  
-      if (isLoggedIn) {
-        isLoggedIn = false;
-        localStorage.setItem("isLoggedIn", "false");
-      } else {
-        isLoggedIn = true;
-        localStorage.setItem("isLoggedIn", "true");
-      }
-  
-      updateAuthText();
-    });
-  
-    function updateAuthText() {
-      authToggle.textContent = isLoggedIn ? "로그아웃" : "로그인";
-    }
-  });
-  
