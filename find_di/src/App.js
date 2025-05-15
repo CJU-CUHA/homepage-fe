2cfcb8c774d639b80f7eadedcb9fc144f16d98d8
test <freetime483@naver.com>
2025-05-15 18:58:45 +0900

    링크걸기기

--- find_di/src/App.js
+++ find_di/src/App.js
@@ -1,34 +1,111 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './img/cu.png';
-import './banner.css';
-
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="main-banner">
-        <div className="logo">
-            <img src={logo} alt="logo" width="30px"></img>
-        </div>
-        <nav class="nav-menu">
-            <span>CTF Time</span>
-            <div className="divider"></div>
-            <span>WAR GAME</span>
-            <div className="divider"></div>
-            <span>CUHA WIKI</span>
-            <div className="divider"></div>
-            <span>MINECRAFT</span>
-            <div className="divider"></div>
-            <span className="click-me">Click Me</span>
-        </nav>
-        <div className="auth">
-            <span className="login">로그인</span>
-            <span className="sep">|</span>
-            <span className="signup">회원가입</span>
-        </div>
-    </header>
-    </div>
+  const [showModal, setShowModal] = useState(false);
+  const [ctfData, setCtfData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
+
+  const fetchCTF = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch('http://cuha.cju.ac.kr:10001/api/api/ctftime');
+      const data = await res.json();
+      setCtfData(data);
+    } catch (err) {
+      console.error('CTF fetch error:', err);
+      setCtfData([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const logoLink = React.createElement(
+    'a',
+    { href: 'klogin.html' },
+    React.createElement('img', { src: logo, alt: 'Logo', draggable: 'false', width: '30px' })
   );
+
+  const ctfLink = React.createElement(
+    'a',
+    { href: '#', onClick: fetchCTF },
+    'CTF Time'
+  );
+
+  const ctfItems = loading
+    ? React.createElement('p', null, '로딩 중...')
+    : ctfData.map((item, idx) =>
+        React.createElement(
+          'div',
+          { key: idx, className: 'ctf-item' },
+          React.createElement('h2', null, item.title),
+          React.createElement('p', null, item.description),
+          React.createElement('p', null, `날짜: ${item.date}`)
+        )
+      );
+
+  const navMenu = React.createElement(
+    'nav',
+    { className: 'banner-section nav-menu' },
+    ctfLink,
+    React.createElement('div', { id: 'ctf-data', className: 'ctf-container' }, ctfItems),
+    React.createElement('a', { href: 'https://cifrar.cju.ac.kr/' }, 'WAR GAME'),
+    React.createElement('a', { href: 'https://cuha.cju.ac.kr/index.php/%EB%8C%80%EB%AC%B8' }, 'CUHA WIKI'),
+    React.createElement('span', null, 'MINECRAFT'),
+    React.createElement(
+      'button',
+      { className: 'click-me', onClick: openModal },
+      'Click Me'
+    )
+  );
+
+  const authSection = React.createElement(
+    'div',
+    { className: 'auth' },
+    React.createElement('a', { href: 'klogin.html', className: 'login' }, '로그인'),
+    React.createElement('span', { className: 'sep' }, '|'),
+    React.createElement('a', { href: 'kmember.html', className: 'signup' }, '회원가입')
+  );
+
+  const modal = showModal
+    ? React.createElement(
+        'div',
+        { className: 'modal', onClick: closeModal },
+        React.createElement(
+          'div',
+          {
+            className: 'modal-content',
+            onClick: (e) => e.stopPropagation(),
+          },
+          React.createElement(
+            'span',
+            { className: 'close', onClick: closeModal },
+            '×'
+          ),
+          React.createElement('p', null, '언제나 당신을 응원합니다.'),
+          React.createElement(
+            'button',
+            { onClick: closeModal },
+            '닫기'
+          )
+        )
+      )
+    : null;
+
+  const header = React.createElement(
+    'header',
+    { className: 'main-banner' },
+    React.createElement('div', { className: 'banner-section logo' }, logoLink),
+    navMenu,
+    authSection
+  );
+
+  return React.createElement('div', { className: 'App' }, header, modal);
 }
 
 export default App;
