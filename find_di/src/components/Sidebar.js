d31d62fcbbf5475cab352cd2245cbfbac4d24c59
test <freetime483@naver.com>
2025-06-23 17:28:35 +0900

    feat: 사이드바 ctftime 추가

--- find_di/src/components/Sidebar.js
+++ find_di/src/components/Sidebar.js
@@ -10,7 +10,7 @@ function Sidebar({ isOpen, onClose }) {
       <div className="sidebar" onClick={(e) => e.stopPropagation()}>
         <h3>메뉴</h3>
         <Link to="/JangbalCity" onClick={onClose}>개발부</Link>
-        <Link to="/ctf" onClick={onClose}>CTF TIME</Link>
+        <Link to="/CTFTime" onClick={onClose}>CTF TIME</Link>
         <Link to="/war" onClick={onClose}>WAR GAME</Link>
         <Link to="https://cuha.cju.ac.kr/index.php/%EB%8C%80%EB%AC%B8" >CUHA WIKI</Link>
         <Link to="https://cifrar.cju.ac.kr" onClick={onClose}>MINECRAFT</Link>
